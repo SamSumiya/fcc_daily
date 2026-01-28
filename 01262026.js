a6ce3095207673caf492e6fe6de88d77a61a00ac
Sam Sumiya <60025491+SamSumiya@users.noreply.github.com>
2026-01-27 16:17:48 -0800

    added jan 26

---
+++ 01262026.js
@@ -0,0 +1,10 @@
+function fizzBuzzMini(n) {
+  if (n % 3 === 0 && n % 5 === 0) return 'FizzBuzz'
+  if ( n % 3 === 0 ) {
+    return 'Fizz'
+  } else if ( n % 5 === 0 ) {
+    return 'Buzz'
+  } else {
+    return String(n) 
+  }
+}
